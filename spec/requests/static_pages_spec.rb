cb4476c0935eaccabdc900cde75b2e33c94e1a6b
Katie Arbuckle <ktj72@hotmail.com>
2014-02-12 05:19:58 +0000

    Misc updates. (Not descriptive. I know. I'll fix later.)

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,20 +1,13 @@
 require 'spec_helper'
 
 describe "StaticPages" do
+  subject { page }
+
   describe "Home Page" do
-    it "should have the content 'Wishlist'" do
-      visit '/static_pages/home'
-      expect(page).to have_content('Wishlist')
-    end
+    before { visit root_path }
 
-    it "should have the base title" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("Wishlist")
-    end
-
-    it "should not have a custom page title" do
-      visit '/static_pages/home'
-      expect(page).not_to have_title('| Home')
-    end
+    it { should have_content('Wishlist') }
+    it { should have_title(full_title('')) }
+    it { should_not have_title('| Home') }
   end
 end
