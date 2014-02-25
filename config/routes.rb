cdba829edcd756e14f7f83ab1bf0d984ab48dd79
katiejo72 <katiejo72@gmail.com>
2014-02-25 05:25:11 +0000

    Mostly finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Wishlist::Application.routes.draw do
-  get "static_pages/home"
+  get "users/new" #will remove this line later
+  root 'static_pages#home'
+  match '/signup', to: 'users#new', via: 'get'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
