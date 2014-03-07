495e955f17d82a42b2b42fc01f1df4bf701c5f08
katiejo72 <katiejo72@gmail.com>
2014-03-07 04:09:37 +0000

    Finished password authentication and validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
+
 	validates :fname, presence: true, length: { maximum: 20 }
 	validates :lname, presence: true, length: { maximum: 20 }
+
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },
 	                  uniqueness: { case_sensitive: false }
-  #has_secure_password   #commented out for TDD
+
+  has_secure_password
+  validates :password, length: { minimum: 8 }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe User do
   
   before { @user = User.new(fname: "Example", lname: "User", email: "user@example.com",
-                            password: "foobar", password_confirmation: "foobar") }
+                            password: "foobarbaz", password_confirmation: "foobarbaz") }
 
   subject { @user }
 
@@ -13,6 +13,7 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:authenticate) }
 
   it { should be_valid }
 
@@ -83,5 +84,26 @@ describe User do
   describe "when password does not match" do
     before { @user.password_confirmation = "mismatch" }
     it { should_not be_valid }
+  end
+
+  describe "with a password that is too short" do
+    before { @user.password = @user.password_confirmation = "a" * 7 }
+    it { should be_invalid }
+  end
+
+  describe "return value of authenticate method" do
+    before { @user.save }
+    let(:found_user) { User.find_by(email: @user.email) }
+
+    describe "with valid password" do
+      it { should eq found_user.authenticate(@user.password) }
+    end
+
+    describe "with invalid password" do
+      let(:user_for_invalid_password) { found_user.authenticate("invalid") }
+
+      it { should_not eq user_for_invalid_password }
+      specify { expect(user_for_invalid_password).to be_false }
+    end
   end
 end
