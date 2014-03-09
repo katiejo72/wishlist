f5ba3fbc0d91047b60cd0b75e8438ac1956fb434
katiejo72 <katiejo72@gmail.com>
2014-03-09 20:42:50 +0000

    Fixed a CSS issue. More begginings of a user page.

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+	factory :user do
+		fname			"Katie"
+		lname			"Arbuckle"
+		email			"katiejo72@gmail.com"
+		password	"foobarbaz"
+		password_confirmation	"foobarbaz"
+	end
+end
